be119d6d55c968fcf33c8e27b303995d43766ee6
bear <bear.darkshien@gmail.com>
2013-07-05 03:52:57 +0000

    update build vim script

--- build_vim_centos.sh
+++ build_vim_centos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# stop on error
 set -e
 
 SCRIPT_NAME=${BASH_SOURCE[0]}
@@ -9,29 +10,40 @@ DATE=$(date -d "today" +"%Y%m%d%H%M")
 echo -e "#"
 echo -e "# $SCRIPT_NAME: install necessary packages"
 echo -e "#"
-sudo apt-get install -y --fix-missing \
-    libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev \
-    gcc python-dev ruby-dev mercurial
+sudo yum install -y \
+    gcc make \
+    ncurses-devel \
+    lua lua-devel \
+    ruby ruby-devel \
+    python python-devel \
+    perl perl-devel perl-ExtUtils-Embed \
+    mercurial
 
 echo -e "#"
 echo -e "# $SCRIPT_NAME: download vim source"
 echo -e "#"
 mkdir -p ~/packages
 cd ~/packages/
-hg clone https://code.google.com/p/vim/
+if [ ! -d "vim" ]; then
+    hg clone https://code.google.com/p/vim/
+else
+    cd vim
+    hg pull
+fi
 
 echo -e "#"
 echo -e "# $SCRIPT_NAME: build vim"
 echo -e "#"
 cd ~/packages/vim/
 ./configure --with-features=huge \
-            --enable-rubyinterp \
+            --enable-multibyte \
+            --disable-selinux \
+            --enable-cscope \
             --enable-pythoninterp \
+            --enable-python3interp \
+            --enable-rubyinterp \
             --enable-perlinterp \
-            --enable-gui=gnome2 \
-            --enable-cscope
+            --enable-luainterp
 make -j 4
 sudo make install
 
@@ -52,12 +64,13 @@ ln -s .vim/.gvimrc
 echo -e "#"
 echo -e "# $SCRIPT_NAME: install depending packages of plugins"
 echo -e "#"
-sudo apt-get install -y python-pip exuberant-ctags
-sudo pip install flake8
+sudo yum install -y python-pip ctags
+sudo pip-python install flake8
 
 vim +BundleInstall +qall
 
-sudo apt-get install -y cmake
+sudo yum install -y cmake28 gcc-c++
+sudo ln -s /usr/bin/cmake28 /usr/local/bin/cmake
 cd ~/.vim/bundle/YouCompleteMe/
 ./install.sh --clang-completer
 
